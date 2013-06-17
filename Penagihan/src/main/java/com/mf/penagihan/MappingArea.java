cc519c9c34de9254ac9cfd945f64e04d9ed9492e
Antonius Suharmono <antonius.suharmono@gmail.com>
2013-06-17 12:48:53 +0700

    pom modified

---
+++ Penagihan/src/main/java/com/mf/penagihan/MappingArea.java
@@ -0,0 +1,14 @@
+package com.mf.penagihan;
+
+import com.mf.common.domain.Kolektor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: antonius
+ * Date: 6/17/13
+ * Time: 12:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MappingArea {
+    Kolektor kolektor = new Kolektor();
+}
